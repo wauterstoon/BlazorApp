d248d4b663c1cdd15a0583f1a547ce1dec1ef1ce
toonw <toonw@NB-WAUTERST-W10>
2022-05-17 10:02:33 +0200

    Add project files.

---
+++ BlazorApp/Shared/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace BlazorApp.Shared.Models
+{
+    public class User
+    {
+        public int Userid { get; set; }
+        public string Username { get; set; } = null!;
+        public string Address { get; set; } = null!;
+        public string Cellnumber { get; set; } = null!;
+        public string Emailid { get; set; } = null!;
+    }
+}
